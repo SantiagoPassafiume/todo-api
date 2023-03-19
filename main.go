bf309c4f02319e417396aa7789c184250d0dc4fe
Santiago Passafiume <santiagopassafiume@protonmail.com>
2023-03-19 19:46:09 -0300

    create server with gin

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 type todo struct {
 	ID	string 	`json:"id"`
 	Item	string `json:"title"`
@@ -10,4 +14,9 @@ var todos = []todo{
 	{ID: "1", Item: "Clean Room", Completed: false},
 	{ID: "2", Item: "Read Book", Completed: false},
 	{ID: "3", Item: "Record Video", Completed: false},
+}
+
+func main(){
+	router := gin.Default()
+	router.Run("localhost:9090")
 }
